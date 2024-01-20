bb2ac715123e12d19a1001bb28529a435d8ab0c5
sixstepsaway <61681613+sixstepsaway@users.noreply.github.com>
2024-01-20 03:57:32 +0000

    Initial Commit

---
+++ ItemListTool/MainScene.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System;
+
+public partial class MainScene : MarginContainer
+{
+
+
+
+
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+
+	private void _on_mod_folder_location_brwse_pressed(){
+
+	}
+
+	private void _on_output_file_location_brwse_pressed(){
+		
+	}
+	private void _on_generate_pressed(){
+		
+	}
+	private void _on_button_github_pressed(){
+		
+	}
+	private void _on_button_ko_fi_pressed(){
+		
+	}
+	private void _on_file_dialog_mods_folder_dir_selected(){
+		
+	}
+	private void _on_file_dialog_mods_folder_canceled(){
+		
+	}
+	private void _on_file_dialog_mods_folder_confirmed(){
+		
+	}
+	private void _on_file_dialog_save_file_dir_selected(){
+		
+	}
+	private void _on_file_dialog_save_file_canceled(){
+		
+	}
+	private void _on_file_dialog_save_file_confirmed(){
+		
+	}
+
+
+
+
+
+
+
+
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
